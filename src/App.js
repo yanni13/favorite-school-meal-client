6214db3a76b7315459b47f9a77b9f0250c3ccf88
asn6878 <asn6878@gmail.com>
2023-10-17 20:15:51 +0900

    feat : 헤더 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import styled from "styled-components"
+import MainPage from "./pages/MainPage"
 import JoinForm from "./components/Auth/JoinForm"
 import JoinPage from "./pages/JoinPage"
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path= "/" element={<MainPage/>}></Route>
         <Route path = "/JoinPage" element={<JoinPage/>}></Route>
+        
       </Routes>
     </BrowserRouter>
   )
