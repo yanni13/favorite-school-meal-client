a4de6f94a6428b0f73bea19d874933ce7f86dc97
asn6878 <asn6878@gmail.com>
2023-10-30 05:00:23 +0900

    feat : kakao 지도 추가

--- src/components/Main/MiniMap.js
+++ src/components/Main/MiniMap.js
@@ -1,22 +1,35 @@
 import styles from "../../styles/Main/MiniMap.module.css";
 import MapLogo from "../../media/Main/MapLogo.svg";
-
+import { useEffect } from "react";
+const { kakao } = window;
 
 export default function MiniMap() {
     const MapTitle = () => {
         return(
             <div className={styles.title}>
                 <img src={MapLogo}/>
-                <h3>게시판</h3>
+                <h3>지도</h3>
             </div>
         );
     }
 
     const MapContent = () => {
+        useEffect(()=>{
+            const container = document.getElementById('map');
+            const options = {
+                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                level: 3
+            };
+            const map = new kakao.maps.Map(container, options);
+        }, [])
+
         return(
             <>
-                <div>
-                    지도 내용~
+                <div id="map" style={{
+                    width: '500px',
+                    height: '500px'
+                }}>
+                    
                 </div>
             </>
         );
