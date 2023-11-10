b291d0c61de3cf77d262c30746e9846154b55b9c
asn6878 <asn6878@gmail.com>
2023-11-11 00:55:51 +0900

    feat : MapInfo 컴포넌트 작성

--- src/components/Map/MainMap.js
+++ src/components/Map/MainMap.js
@@ -1,16 +1,52 @@
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { useState } from 'react';
+import MapInfo from './MapInfo';
+import styled from 'styled-components';
 
+const locations = [
+	{ title: '다이소', latlng: { lat: 35.83612503877446, lng: 128.7527715858539 } },
+	{ title: '아틀리에빈', latlng: { lat: 35.83642690671568, lng: 128.752617763627 } },
+	{ title: '텃밭', latlng: { lat: 33.450879, lng: 126.56994 } },
+	{ title: '근린공원', latlng: { lat: 33.451393, lng: 126.570738 } },
+];
 
+const S = {
+	InfoWrapper: styled.div`
+		display: flex;
+		position : fixed;
+		bottom: 25px;
+		left: 33px;
+		z-index: 1;
 
+	`
+}
 function MainMap(){
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
-		<Map 
-          center={{ lat: 35.8361601, lng: 128.7528893 }} 
-          style={{ width: '100%', height: '100%' }}
-          level={5} 
-        >
-			<MapMarker position={{ lat:  35.83658135778566, lng: 128.75299465624974 }}> </MapMarker> // 마커 좌표
-		</Map>
+		<>
+			<Map 
+			center={{ lat: 35.8361601, lng: 128.7528893 }} 
+			style={{ width: '390px', height: '844px' }}
+			level={3} 
+			>
+				{locations.map((loc, idx) => (
+					<MapMarker
+						clickable={true}
+						onClick={() => setIsOpen(true)}
+						position={loc.latlng}
+						image={{
+							src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+							size: { width: 24, height: 35 },
+						}}
+						title={loc.title}
+					/>
+				))}
+				<S.InfoWrapper>
+					<MapInfo/>
+				</S.InfoWrapper>
+			</Map>
+		</>
 	);
 }
 
---
+++ src/components/Map/MapInfo.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import styled from "styled-components";
+
+const S = {
+    InfoBox : styled.div`
+        width: 330px;
+        height: 210px;
+        background-color: aqua;
+    `
+}
+
+function MapInfo() {
+    return (
+        <>
+            <S.InfoBox>
+
+            </S.InfoBox>
+        </>
+    );
+}
+
+export default MapInfo;
